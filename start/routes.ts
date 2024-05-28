4c2e087b0a16dc0f38137f52fe4507ac3f14f786
Yifan <yifanyemontpe@gmail.com>
2024-05-28 10:41:52 +0200

    Add logout service

--- start/routes.ts
+++ start/routes.ts
@@ -9,6 +9,8 @@
 
 const RegisterUserController = () => import('#controllers/auth/register_user_controller')
 const LoginController = () => import('#controllers/auth/login_controller')
+import LogoutController from '#controllers/auth/logout_controller'
+import { middleware } from '#start/kernel'
 import router from '@adonisjs/core/services/router'
 
 // Global matcher
@@ -20,5 +22,11 @@ router
   .group(() => {
     router.post('/register', [RegisterUserController])
     router.post('/login', [LoginController])
+
+    router.post('/logout', [LogoutController]).use(
+      middleware.auth({
+        guards: ['api'],
+      })
+    )
   })
   .prefix('/api')
