3244225d9737db616a26ad659c015a4a3c7f541f
Yifan <yifanyemontpe@gmail.com>
2024-05-30 11:54:48 +0200

    Refactor using TemplateService

--- start/routes/private/templates.routes.ts
+++ start/routes/private/templates.routes.ts
@@ -1,6 +1,7 @@
 import CreateTemplateController from '#controllers/templates/create_template.controller'
 import GetTemplateController from '#controllers/templates/get_template.controller'
 import GetTemplatesListController from '#controllers/templates/get_templates_list.controller'
+import UpdateTemplateController from '#controllers/templates/update_template.controller'
 import { middleware } from '#start/kernel'
 import router from '@adonisjs/core/services/router'
 
@@ -9,6 +10,8 @@ router
     router.get('/', [GetTemplatesListController])
     router.post('/', [CreateTemplateController])
     router.get('/:id', [GetTemplateController])
+    router.put('/:id', [UpdateTemplateController])
+    router.delete('/:id', [UpdateTemplateController])
   })
   .prefix('/api/templates')
   .use(
