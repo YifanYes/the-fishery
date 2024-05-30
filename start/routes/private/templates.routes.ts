8f2f3f502572dc69f0f7e6757a481403e9d05627
Yifan <yifanyemontpe@gmail.com>
2024-05-30 12:00:33 +0200

    Add delete template service

--- start/routes/private/templates.routes.ts
+++ start/routes/private/templates.routes.ts
@@ -1,4 +1,5 @@
 import CreateTemplateController from '#controllers/templates/create_template.controller'
+import DeleteTemplateController from '#controllers/templates/delete_template.controller'
 import GetTemplateController from '#controllers/templates/get_template.controller'
 import GetTemplatesListController from '#controllers/templates/get_templates_list.controller'
 import UpdateTemplateController from '#controllers/templates/update_template.controller'
@@ -11,7 +12,7 @@ router
     router.post('/', [CreateTemplateController])
     router.get('/:id', [GetTemplateController])
     router.put('/:id', [UpdateTemplateController])
-    router.delete('/:id', [UpdateTemplateController])
+    router.delete('/:id', [DeleteTemplateController])
   })
   .prefix('/api/templates')
   .use(
